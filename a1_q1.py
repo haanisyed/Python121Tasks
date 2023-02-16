7d4ae720947935b1aa2fbc280fd8a7624d6d97e4
Haani <116673121+haanisyed@users.noreply.github.com>
2023-02-16 09:16:31 -0500

    Update a1_q1.py

--- a1_q1.py
+++ a1_q1.py
@@ -1,15 +1,7 @@
 '''
-CISC-121 2023W
-
+Winter 2023
 Name: Haani Syed
-
-Student Number: 20331181
-
-Email: 21ahs7@queensu.ca
-
 Date: 2023-01-19
-
-I confirm that this assignment solution is my own work and conforms to Queen’s standards of Academic Integrity
 '''
 
 import random #importing random
